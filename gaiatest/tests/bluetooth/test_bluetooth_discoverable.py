088ae45fb1375a5f483e82b3b69f1b5188b46232
rwood-moz <rwood@mozilla.com>
2013-04-27 14:53:30 -0400

    Fixes for using methods in pull 600

--- gaiatest/tests/bluetooth/test_bluetooth_discoverable.py
+++ gaiatest/tests/bluetooth/test_bluetooth_discoverable.py
@@ -19,21 +19,22 @@ class TestBluetoothDiscoverable(GaiaTestCase):
         # Bluetooth host object
         self.bluetooth_host = BluetoothHost(self.marionette)
 
-        # If bluetooth is enabled on the device turn it off to ensure no existing connections
-        self.data_layer.disable_bluetooth(self)
-
-        # Enable device-side bluetooth
-        self.data_layer.enable_bluetooth(self)
+        # Disable/enable device bluetooth in case are any active connections
+        self.data_layer.bt_disable_bluetooth()
+        self.assertFalse(self.data_layer.bt_is_bluetooth_enabled)
+        self.data_layer.bt_enable_bluetooth()
         self.assertTrue(self.data_layer.bt_is_bluetooth_enabled)
+        time.sleep(30)
 
         # Remove any existing device pairings so we are starting clean
-        self.data_layer.unpair_all_bluetooth_devices(self)
+        self.data_layer.bt_unpair_all_bluetooth_devices()
 
     def test_discoverable(self):
         # Set our device's bluetooth name uniquely so we can identify it
         device_name = str(time.time())
         self.marionette.log("Setting device's bluetooth name to '%s'" % device_name)
         self.data_layer.bt_set_device_bluetooth_name(device_name)
+        time.sleep(5)
 
         # Place our device in discoverable mode so it can be found by host machine
         self.marionette.log("Setting device discoverable mode ON")
@@ -46,12 +47,13 @@ class TestBluetoothDiscoverable(GaiaTestCase):
         # Take the device out of discoverable mode
         self.marionette.log("Setting device discoverable mode OFF")
         self.data_layer.bt_set_device_bluetooth_discoverable(False)
+        time.sleep(10)
 
         # Now have host machine inquire and shouldn't find our device
-        time.sleep(5)
         device_found = self.bluetooth_host.is_device_visible(device_name)
         self.assertFalse(device_found, "Host shouldn't see our device (device discoverable mode is OFF)")
 
         # Disable device-side bluetooth
-        self.data_layer.disable_bluetooth(self)
+        self.data_layer.bt_disable_bluetooth()
         self.assertFalse(self.data_layer.bt_is_bluetooth_enabled)
+
