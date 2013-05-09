41d703f6dcc076b0ef7415a364b58cc237ebfafe
Bob Silverberg <bob.silverberg@gmail.com>
2013-05-09 16:44:08 -0400

    Fix test_dialer_add_contact by remove extra step

--- gaiatest/tests/dialer/test_dialer_add_contact.py
+++ gaiatest/tests/dialer/test_dialer_add_contact.py
@@ -60,11 +60,6 @@ class TestDialerAddContact(GaiaTestCase):
         add_new_contact = self.marionette.find_element(*self._add_new_contact_button_locator)
         self.marionette.tap(add_new_contact)
 
-        # Tap on "Create New Contact"
-        self.wait_for_element_displayed(*self._create_new_contact_locator)
-        create_new_contact = self.marionette.find_element(*self._create_new_contact_locator)
-        self.marionette.tap(create_new_contact)
-
         # Switch to add contacts frame
         self.marionette.switch_to_frame()
         self.wait_for_element_present(*self._new_contact_frame_locator)
