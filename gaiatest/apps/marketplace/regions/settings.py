e565ab290043d016b2a8e8c21d04a28cd9248a24
zacc <zcampbell@mozilla.com>
2013-07-11 16:42:54 +0100

    add test for change user's region
    Conflicts:
    
    	gaiatest/tests/marketplace/manifest.ini

--- gaiatest/apps/marketplace/regions/settings.py
+++ gaiatest/apps/marketplace/regions/settings.py
@@ -8,11 +8,16 @@ from gaiatest.apps.base import PageRegion
 
 class Settings(Base):
 
+    name = 'Marketplace Dev'
+
     _email_account_field_locator = ('id', 'email')
     _save_locator = ('css selector', 'footer > p > button')
     _sign_in_button_locator = ('css selector', 'a.button.persona')
     _sign_out_button_locator = ('css selector', 'a.button.logout')
     _back_button_locator = ('id', 'nav-back')
+    _region_select_locator = ('id', 'region')
+    _region_select_value_locator = ('css selector', '#region option[selected]')
+    _save_changes_button_locator = ('xpath', "//section[@id='account-settings']//button[text()='Save Changes']")
 
     def __init__(self, marionette):
         Base.__init__(self, marionette)
@@ -39,6 +44,17 @@ class Settings(Base):
         # TODO: remove scrollIntoView hack
         self.marionette.execute_script("arguments[0].scrollIntoView(false);", [sign_out_button])
         sign_out_button.tap()
+
+    def select_region(self, region):
+        self.marionette.find_element(*self._region_select_locator).tap()
+        self.select(region)
+
+    def tap_save_changes(self):
+        self.marionette.find_element(*self._save_changes_button_locator).tap()
+
+    @property
+    def region(self):
+        return self.marionette.find_element(*self._region_select_value_locator).text
 
     @property
     def email(self):
