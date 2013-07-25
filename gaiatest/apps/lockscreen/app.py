2051975aafc49e0767a3860f0b5ed4aaca457205
bebef1987 <florin.strugariu@softvision.ro>
2013-07-25 17:37:00 +0300

    lock screen app move to lockscreen app

---
+++ gaiatest/apps/lockscreen/app.py
@@ -0,0 +1,56 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from gaiatest.apps.base import Base
+from marionette.marionette import Actions
+
+
+class LockScreen(Base):
+
+    _lockscreen_locator = ('id', 'lockscreen')
+    _lockscreen_icon_area_locator = ('id', 'lockscreen-icon-container')
+
+    _lockscreen_handle_locator = ('id', 'lockscreen-area-handle')
+    _unlock_button_locator = ('id', 'lockscreen-area-unlock')
+    _camera_button_locator = ('id', 'lockscreen-area-camera')
+
+    _camera_frame_locator = ('css selector', 'iframe[src*="camera"][src*="/index.html"]')
+
+    def swipe_to_unlock(self):
+
+        unlock_handle = self.marionette.find_element(*self._lockscreen_handle_locator)
+        unlock_handle_x_centre = int(unlock_handle.size['width'] / 2)
+        unlock_handle_y_centre = int(unlock_handle.size['height'] / 2)
+
+        # Get the end position from the demo animation
+        lock_screen_icon_area = self.marionette.find_element(*self._lockscreen_icon_area_locator)
+        end_animation_position = lock_screen_icon_area.size['height'] - unlock_handle.size['height']
+
+        # Flick from unlock handle to (0, -end_animation_position) over 800ms duration
+        Actions(self.marionette).flick(unlock_handle, unlock_handle_x_centre, unlock_handle_y_centre, 0, 0 - end_animation_position).perform()
+
+        # Wait for the svg to animate and handle to disappear
+        # TODO add assertion that unlock buttons are visible after bug 813561 is fixed
+        self.wait_for_condition(lambda m: not unlock_handle.is_displayed())
+
+    def tap_unlock_button(self):
+        self.marionette.find_element(*self._unlock_button_locator).tap()
+        # TODO return home screen app here
+
+    def tap_camera_button(self):
+        self.marionette.find_element(*self._camera_button_locator).tap()
+        self.wait_for_lockscreen_not_visible()
+
+        self.marionette.switch_to_frame()
+        self.wait_for_element_present(*self._camera_frame_locator)
+        self.marionette.switch_to_frame(self.marionette.find_element(*self._camera_frame_locator))
+
+        from gaiatest.apps.camera.app import Camera
+        return Camera(self.marionette)
+
+    def wait_for_lockscreen_not_visible(self):
+        self.wait_for_condition(lambda m: not self.marionette.find_element(*self._lockscreen_locator).location['x'] == 0, message="Lockscreen still visible after unlock")
+
+    def wait_for_lockscreen_handle_visible(self):
+        self.wait_for_element_displayed(*self._lockscreen_handle_locator)
--- gaiatest/tests/lockscreen/test_lockscreen_unlock_to_camera.py
+++ gaiatest/tests/lockscreen/test_lockscreen_unlock_to_camera.py
@@ -3,20 +3,11 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from gaiatest import GaiaTestCase
-from marionette.marionette import Actions
+from gaiatest.apps.lockscreen.app import LockScreen
 
 
 class TestLockScreen(GaiaTestCase):
-
-    # Lockscreen area locators
-    _lockscreen_locator = ('id', 'lockscreen')
-    _lockscreen_icon_area_locator = ('id', 'lockscreen-icon-container')
-    _lockscreen_handle_locator = ('id', 'lockscreen-area-handle')
-    _camera_button_locator = ('id', 'lockscreen-area-camera')
-
-    # Camera locators
     _camera_frame_locator = ('css selector', 'iframe[src^="app://camera"][src$="index.html"]')
-    _capture_button_locator = ('id', 'capture-button')
 
     def setUp(self):
         GaiaTestCase.setUp(self)
@@ -26,42 +17,15 @@ class TestLockScreen(GaiaTestCase):
 
         # this time we need it locked!
         self.lockscreen.lock()
-        self.wait_for_element_displayed(*self._lockscreen_handle_locator)
+
+        self.lock_screen = LockScreen(self.marionette)
+        self.lock_screen.wait_for_lockscreen_handle_visible()
 
     def test_unlock_swipe_to_camera(self):
         # https://moztrap.mozilla.org/manage/case/2460/
 
-        self._swipe_and_unlock()
-
-        camera_button = self.marionette.find_element(*self._camera_button_locator)
-        camera_button.tap()
-
-        lockscreen_element = self.marionette.find_element(*self._lockscreen_locator)
-        self.wait_for_condition(lambda m: not self.marionette.find_element(*self._lockscreen_locator).is_displayed())
-
-        self.assertFalse(lockscreen_element.is_displayed(), "Lockscreen still visible after unlock")
-
-        camera_frame = self.marionette.find_element(*self._camera_frame_locator)
-
-        # TODO I would prefer to check visibility of the the frame at this point but bug 813583
-        self.marionette.switch_to_frame(camera_frame)
+        self.lock_screen.swipe_to_unlock()
+        camera = self.lock_screen.tap_camera_button()
 
         # Wait fot the capture button displayed. no need to take a photo.
-        self.wait_for_element_displayed(*self._capture_button_locator)
-
-    def _swipe_and_unlock(self):
-
-        unlock_handle = self.marionette.find_element(*self._lockscreen_handle_locator)
-        unlock_handle_x_centre = int(unlock_handle.size['width'] / 2)
-        unlock_handle_y_centre = int(unlock_handle.size['height'] / 2)
-
-        # Get the end position from the demo animation
-        lockscreen_icon_area = self.marionette.find_element(*self._lockscreen_icon_area_locator)
-        end_animation_position = lockscreen_icon_area.size['height'] - unlock_handle.size['height']
-
-        # Flick from unlock handle to (0, -end_animation_position) over 800ms duration
-        Actions(self.marionette).flick(unlock_handle, unlock_handle_x_centre, unlock_handle_y_centre, 0, 0 - end_animation_position).perform()
-
-        # Wait for the svg to animate and handle to disappear
-        # TODO add assertion that unlock buttons are visible after bug 813561 is fixed
-        self.wait_for_condition(lambda m: not unlock_handle.is_displayed())
+        camera.wait_for_camera_ready()
