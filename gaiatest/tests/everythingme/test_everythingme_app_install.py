be47f1e23ec9ac67e03ef691d56b462258db51b5
zacc <zcampbell@mozilla.com>
2013-07-29 04:18:57 -0700

    everything me app
    Conflicts:
    
    	gaiatest/tests/everythingme/test_everythingme_app_install.py
    	gaiatest/tests/everythingme/test_everythingme_search.py
    	gaiatest/tests/everythingme/test_everythingme_search_accented.py

--- gaiatest/tests/everythingme/test_everythingme_app_install.py
+++ gaiatest/tests/everythingme/test_everythingme_app_install.py
@@ -2,86 +2,51 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from marionette.marionette import Actions
-
 from gaiatest import GaiaTestCase
-
+from gaiatest.apps.everythingme.app import EverythingMe
 
 class TestEverythingMeInstallApp(GaiaTestCase):
-
-    app_installed = False
-
-    # Everything.Me locators
-    _shortcut_items_locator = ('css selector', '#shortcuts-items li')
-    _apps_icon_locator = ('css selector', 'div.evme-apps li.cloud')
 
     # Homescreen locators
     _homescreen_frame_locator = ('css selector', 'div.homescreen > iframe')
     _homescreen_icon_locator = ('css selector', 'li.icon[aria-label="%s"]')
 
-    # Modal dialog locators
-    _modal_dialog_message_locator = ('id', 'modal-dialog-confirm-message')
-    _modal_dialog_ok_locator = ('id', 'modal-dialog-confirm-ok')
-
     def setUp(self):
         GaiaTestCase.setUp(self)
-
-        # Turn off geolocation prompt
         self.apps.set_permission('Homescreen', 'geolocation', 'deny')
-
         self.connect_to_network()
+        self.everythingme = EverythingMe(self.marionette)
 
     def test_installing_everything_me_app(self):
         # https://github.com/mozilla/gaia-ui-tests/issues/67
 
-        # swipe to Everything.Me
-        hs_frame = self.marionette.find_element(*self._homescreen_frame_locator)
-        self.marionette.switch_to_frame(hs_frame)
+        self.everythingme.go_to_everything_me()
 
-        # We'll use js to flick pages for reliability/Touch is unreliable
-        self.marionette.execute_script("window.wrappedJSObject.GridManager.goToPreviousPage();")
+        self.everythingme.wait_for_categories_present()
+        self.assertGreater(self.everythingme.categories_count, 0, 'No shortcut categories found')
+        self.everythingme.categories[0].tap()
 
-        # check for the available shortcut categories
-        self.wait_for_element_displayed(*self._shortcut_items_locator)
-
-        shortcuts = self.marionette.find_elements(*self._shortcut_items_locator)
-        self.assertGreater(len(shortcuts), 0, 'No shortcut categories found')
-
-        # Tap on the first category of shortcuts
-        shortcuts[0].tap()
-
-        self.wait_for_element_displayed(*self._apps_icon_locator)
-
-        first_app_icon = self.marionette.find_element(*self._apps_icon_locator)
-        self.first_app_name = first_app_icon.text
-        Actions(self.marionette).long_press(first_app_icon, 2).perform()
-
-        self.marionette.switch_to_frame()
-
-        self.wait_for_element_displayed(*self._modal_dialog_ok_locator)  # wait for the modal dialog
-
-        modal_dialog_message = self.marionette.find_element(*self._modal_dialog_message_locator).text
-
-        self.first_app_name = modal_dialog_message[
-            modal_dialog_message.find('Add') + 3:
-            modal_dialog_message.find('to Home Screen?')
-        ].strip()  # TODO remove hack after Bug 845828 lands in V1-train
-
-        self.marionette.find_element(*self._modal_dialog_ok_locator).tap()
+        self.everythingme.wait_for_app_icons_displayed()
+        app_name = self.everythingme.app_icons[0].tap_to_install()
 
         # return to home screen
+        hs_frame = self.marionette.find_element(*self._homescreen_frame_locator)
         self.marionette.execute_script("window.wrappedJSObject.dispatchEvent(new Event('home'));")
-
         self.marionette.switch_to_frame(hs_frame)
 
-        # check whether app is installed
-        while self._homescreen_has_more_pages:
-            self._go_to_next_page()
-            if self.is_element_displayed(self._homescreen_icon_locator[0], self._homescreen_icon_locator[1] % self.first_app_name):
-                self.app_installed = True
-                break
+        self.assertTrue(self.is_app_installed(app_name),
+                        'The app %s was not found to be installed on the home screen.' % app_name)
 
-        self.assertTrue(self.app_installed, 'The app %s was not found to be installed on the home screen.' % self.first_app_name)
+    def is_app_installed(self, app_name):
+        """Checks whether app is installed"""
+        is_installed = False
+        while self._homescreen_has_more_pages:
+            if self.is_element_displayed(self._homescreen_icon_locator[0],
+                                         self._homescreen_icon_locator[1] % app_name):
+                is_installed = True
+                break
+            self._go_to_next_page()
+        return is_installed
 
     def _go_to_next_page(self):
         self.marionette.execute_script('window.wrappedJSObject.GridManager.goToNextPage()')
--- gaiatest/tests/everythingme/test_everythingme_search_accented.py
+++ gaiatest/tests/everythingme/test_everythingme_search_accented.py
@@ -6,57 +6,25 @@
 from marionette.keys import Keys
 
 from gaiatest import GaiaTestCase
+from gaiatest.apps.everythingme.app import EverythingMe
 
 
 class TestEverythingMeSearchAccented(GaiaTestCase):
-
-    # Everything.Me locators
-    _shortcut_items_locator = ('css selector', '#shortcuts-items li')
-    _search_box_locator = ('id', 'search-q')
-    _search_title_locator = ('id', 'search-title')
-    _loading_apps_locator = ('css selector', 'div.loading-apps')
-
-    # Homescreen locators
-    _homescreen_frame_locator = ('css selector', 'div.homescreen > iframe')
-
-    # Search string
-    _test_string = u"Özdemir Erdoğan"
-    _test_string_encoded = u'\xd6zdemir Erdo\u011fan'
 
     def setUp(self):
         GaiaTestCase.setUp(self)
         self.apps.set_permission('Homescreen', 'geolocation', 'deny')
         self.connect_to_network()
+        self.everythingme = EverythingMe(self.marionette)
 
-    def test_launch_everything_me_search_accented(self):
-        # This test does a search with accented characters and asserts the
-        # title and shortcut results are returned correctly.
+    def test_launch_everything_me_search(self):
+        self.everythingme.go_to_everything_me()
 
-        # swipe to Everything.Me
-        hs_frame = self.marionette.find_element(*self._homescreen_frame_locator)
-        self.marionette.switch_to_frame(hs_frame)
+        self.everythingme.wait_for_search_box_displayed()
+        self.everythingme.type_into_search_box(u'Özdemir Erdoğan')
+        self.everythingme.wait_for_search_tips_displayed()
+        self.everythingme.tap_search_tip()
 
-        # We'll use js to flick pages for reliability/Touch is unreliable
-        self.marionette.execute_script("window.wrappedJSObject.GridManager.goToPreviousPage();")
-        self.wait_for_condition(lambda m: m.find_element('tag name', 'body')
-            .get_attribute('data-transitioning') != 'true')
-
-        # Find the search box and clear it
-        self.wait_for_element_displayed(*self._search_box_locator)
-        search_input = self.marionette.find_element(*self._search_box_locator)
-        search_input.clear()
-
-        # Enter the string to search
-        search_input.send_keys(self._test_string)
-        search_input.send_keys(Keys.RETURN)
-
-        # Wait for the title to appear and assert it
-        self.wait_for_element_displayed(*self._search_title_locator)
-        title_text = self.marionette.find_element(*self._search_title_locator).text
-        self.assertIn(self._test_string_encoded.lower(), title_text)
-
-        # Check that shortcuts are present. Loading spinner may still be showing
-        self.wait_for_element_not_displayed(*self._loading_apps_locator)
-
-        shortcuts = self.marionette.find_elements(*self._shortcut_items_locator)
-        self.assertGreater(len(shortcuts), 0, 'No shortcut categories found')
+        self.everythingme.wait_for_categories_present()
+        self.assertGreater(self.everythingme.categories_count, 0, 'No shortcut categories found')
+        self.keyboard.tap_enter()
