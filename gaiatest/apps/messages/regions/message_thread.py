4f8ef912cea2f3eed9a4f2a4e1173cf672f708fc
bebef1987 <florin.strugariu@softvision.ro>
2013-08-05 16:50:19 +0300

    migrate Messages app to marionette By

--- gaiatest/apps/messages/regions/message_thread.py
+++ gaiatest/apps/messages/regions/message_thread.py
@@ -2,14 +2,15 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from marionette.by import By
 from gaiatest.apps.base import Base
 from gaiatest.apps.base import PageRegion
 
 
 class MessageThread(Base):
 
-    _all_messages_locator = ('css selector', '#messages-container li.message')
-    _received_message_content_locator = ('css selector', "#messages-container li.message.received")
+    _all_messages_locator = (By.CSS_SELECTOR, '#messages-container li.message')
+    _received_message_content_locator = (By.CSS_SELECTOR, "#messages-container li.message.received")
 
     def wait_for_received_messages(self):
         self.wait_for_element_displayed(*self._received_message_content_locator, timeout=180)
@@ -24,7 +25,7 @@ class MessageThread(Base):
 
 
 class Message(PageRegion):
-    _text_locator = ('css selector', '.bubble > p')
+    _text_locator = (By.CSS_SELECTOR, '.bubble > p')
 
     @property
     def text(self):
