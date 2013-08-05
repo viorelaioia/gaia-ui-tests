be50724a8963b935736f69dd8fa80f42e3312dc6
Teodosia Pop <teodosia.pop@gmail.com>
2013-08-05 16:24:24 +0300

    Migrate gallery app to use marionette By

--- gaiatest/tests/gallery/test_gallery_edit_photo.py
+++ gaiatest/tests/gallery/test_gallery_edit_photo.py
@@ -2,15 +2,16 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from marionette.by import By
 from gaiatest import GaiaTestCase
 from gaiatest.apps.gallery.app import Gallery
 
 
 class TestGalleryEditPhoto(GaiaTestCase):
 
-    _edit_effect_button_locator = ('id', 'edit-effect-button')
-    _effect_options_locator = ('css selector', '#edit-effect-options a')
-    _edit_save_locator = ('id', 'edit-save-button')
+    _edit_effect_button_locator = (By.ID, 'edit-effect-button')
+    _effect_options_locator = (By.CSS_SELECTOR, '#edit-effect-options a')
+    _edit_save_locator = (By.ID, 'edit-save-button')
 
     def setUp(self):
         GaiaTestCase.setUp(self)
