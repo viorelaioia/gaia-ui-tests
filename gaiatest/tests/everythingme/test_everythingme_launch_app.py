86e7b85f3dc2f911646b771e41211140359b67ff
Zac Campbell <zcampbell@mozilla.com>
2013-08-08 17:53:55 +0100

    Migrate e.me app to use the new search and remove the old app object
    Conflicts:
    	gaiatest/tests/everythingme/manifest.ini

--- gaiatest/tests/everythingme/test_everythingme_launch_app.py
+++ gaiatest/tests/everythingme/test_everythingme_launch_app.py
@@ -3,7 +3,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from gaiatest import GaiaTestCase
-from gaiatest.apps.everythingme.app import EverythingMe
+from gaiatest.apps.homescreen.app import Homescreen
 
 
 class TestEverythingMeLaunchApp(GaiaTestCase):
@@ -12,19 +12,20 @@ class TestEverythingMeLaunchApp(GaiaTestCase):
         GaiaTestCase.setUp(self)
         self.apps.set_permission('Homescreen', 'geolocation', 'deny')
         self.connect_to_network()
-        self.everythingme = EverythingMe(self.marionette)
 
     def test_launch_everything_me_app(self):
         # https://github.com/mozilla/gaia-ui-tests/issues/69
 
         app_name = 'Twitter'
-        self.everythingme.go_to_everything_me()
+        homescreen = Homescreen(self.marionette)
+        homescreen.launch()
 
-        self.everythingme.wait_for_categories_to_load()
-        self.assertGreater(self.everythingme.categories_count, 0, 'No shortcut categories found')
-        self.everythingme.tap_category('social')
+        search_panel = homescreen.tap_search_bar()
+        search_panel.type_into_search_box(app_name)
+        search_panel.wait_for_everything_me_results_to_load()
 
-        self.everythingme.wait_for_app_icons_displayed()
-        self.everythingme.tap_app_icon(app_name)
+        self.assertGreater(search_panel.everything_me_apps_count, 0)
+
+        search_panel.results[0].tap()
 
         self.assertIn(app_name, self.marionette.title)
