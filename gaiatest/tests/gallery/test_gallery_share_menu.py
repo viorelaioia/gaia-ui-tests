e3a6c2a3afa5df5b92d0210dacb3a53e58911396
Sasha Kruglov <sashakruglov@hotmail.com>
2013-08-31 03:17:57 +0400

    migrate gallery share test to use actions

--- gaiatest/tests/gallery/test_gallery_share_menu.py
+++ gaiatest/tests/gallery/test_gallery_share_menu.py
@@ -2,67 +2,33 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from marionette.by import By
 from gaiatest import GaiaTestCase
+from gaiatest.apps.gallery.app import Gallery
 
 
 class TestGalleryShareMenu(GaiaTestCase):
 
-    _gallery_items_locator = (By.CSS_SELECTOR, 'li.thumbnail')
-    _current_image_locator = (By.CSS_SELECTOR, '#frame2 > img')
-    _photos_toolbar_locator = (By.ID, 'fullscreen-toolbar')
-    _share_button_locator = (By.ID, 'fullscreen-share-button')
-    _back_button_locator = (By.ID, 'fullscreen-back-button')
-
-    _share_with_list_locator = (By.CSS_SELECTOR, '#list-menu-root a[role="button"]')
-    _cancel_button_locator = (By.CSS_SELECTOR, '#list-menu-root button[data-action="cancel"]')
-
     def setUp(self):
         GaiaTestCase.setUp(self)
-
-        # add photo to storage
         self.push_resource('IMG_0001.jpg', destination='DCIM/100MZLLA')
 
-        # launch the Gallery app
-        self.app = self.apps.launch('Gallery')
-
     def test_gallery_click_share_button(self):
-        self.wait_for_element_displayed(*self._gallery_items_locator)
+        gallery = Gallery(self.marionette)
+        gallery.launch()
 
-        first_gallery_item = self.marionette.find_elements(*self._gallery_items_locator)[0]
+        gallery.wait_for_thumbnails_to_load()
+        image = gallery.tap_first_gallery_item()
 
-        first_gallery_item.tap()
-        self.wait_for_element_displayed(*self._current_image_locator)
-
-        current_image = self.marionette.find_element(*self._current_image_locator)
-        photos_toolbar = self.marionette.find_element(*self._photos_toolbar_locator)
-
-        self.assertIsNotNone(current_image.get_attribute('src'))
-        self.assertTrue(photos_toolbar.is_displayed())
+        self.assertIsNotNone(image.current_image_source)
+        self.assertTrue(image.is_photo_toolbar_displayed)
 
         # click on share button and check the element is correct
-        self.marionette.find_element(*self._share_button_locator).tap()
+        actions_list = image.tap_share_button()
+        self.assertGreater(actions_list.options_count, 1)
+        actions_list.tap_cancel()
 
-        # switch to home frame and check the result
-        self.marionette.switch_to_frame()
+        gallery = image.tap_tile_view_button()
+        gallery.wait_for_thumbnails_to_load()
 
-        # wait for Share with Menu is rendered
-        self.wait_for_element_displayed(*self._share_with_list_locator)
-        share_with_list = self.marionette.find_elements(*self._share_with_list_locator)
-        self.wait_for_element_displayed(*self._cancel_button_locator)
-
-        self.assertGreater(len(share_with_list), 0)
-
-        self.marionette.find_element(*self._cancel_button_locator).tap()
-
-        self.marionette.switch_to_frame(self.app.frame_id)
-
-        self.wait_for_element_displayed(*self._back_button_locator)
-        self.marionette.find_element(*self._back_button_locator).tap()
-
-        self.wait_for_element_displayed(*self._gallery_items_locator)
-        first_gallery_item = self.marionette.find_elements(*self._gallery_items_locator)[0]
-        self.assertTrue(first_gallery_item.is_displayed())
-
-    def tearDown(self):
-        GaiaTestCase.tearDown(self)
+        image = gallery.tap_first_gallery_item()
+        self.assertTrue(image.is_image_displayed)
