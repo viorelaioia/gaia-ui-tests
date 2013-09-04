316bb8fd771622873a7dcd52d7eff69a788b8c7a
Teodosia Pop <teodosia.pop@gmail.com>
2013-09-04 14:35:04 +0300

    Migrate contacts/test_sms_contact.py to app object

--- gaiatest/tests/contacts/test_sms_contact.py
+++ gaiatest/tests/contacts/test_sms_contact.py
@@ -2,7 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from marionette.by import By
 from gaiatest import GaiaTestCase
 from gaiatest.mocks.mock_contact import MockContact
 
@@ -10,12 +9,6 @@ from gaiatest.apps.contacts.app import Contacts
 
 
 class TestContacts(GaiaTestCase):
-
-    _sms_app_iframe_locator = (By.CSS_SELECTOR, 'iframe[src^="app://sms"][src$="index.html"]')
-
-    #SMS app locators
-    _recipients_list_locator = (By.CSS_SELECTOR, '#messages-recipients-list span.recipient')
-    _contact_carrier_locator = (By.ID, 'contact-carrier')
 
     def setUp(self):
         GaiaTestCase.setUp(self)
@@ -25,34 +18,21 @@ class TestContacts(GaiaTestCase):
 
     def test_sms_contact(self):
         # https://moztrap.mozilla.org/manage/case/1314/
-        # Setup a text message from a contact
+        # Setup a text message from a contact.
 
         contacts = Contacts(self.marionette)
         contacts.launch()
         contacts.wait_for_contacts()
 
-        # tap on the created contact
+        # Tap on the created contact.
         contact_details = contacts.contact(self.contact['givenName']).tap()
-
-        contact_details.tap_send_sms()
-
-        self.marionette.switch_to_frame()
-
-        sms_iframe = self.marionette.find_element(*self._sms_app_iframe_locator)
-        self.marionette.switch_to_frame(sms_iframe)
+        new_message = contact_details.tap_send_sms()
+        new_message.wait_for_recipients_displayed()
 
         expected_name = self.contact['givenName'] + " " + self.contact['familyName']
         expected_tel = self.contact['tel']['value']
 
-        # Wait for two recipients listed - the last javascript performed on the page
-        # One is from contacts app and one is the manual entry option
-        self.wait_for_condition(lambda m: len(m.find_elements(*self._recipients_list_locator)) == 2)
-
-        # Now check the first listed is from contacts app
-        recipients_list = self.marionette.find_elements(*self._recipients_list_locator)
-        first_recipient = recipients_list[0]
-
-        # Name and phone number have been passed in correctly
-        self.assertEqual(first_recipient.text, expected_name)
-        self.assertEqual(first_recipient.get_attribute('data-number'),
-                         expected_tel)
+        # Now check the first listed is from contacts app.
+        # Name and phone number have been passed in correctly.
+        self.assertEqual(new_message.first_recipient_name, expected_name)
+        self.assertEqual(new_message.first_recipient_number_attribute, expected_tel)
